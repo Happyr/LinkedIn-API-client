7626ce43d9d5f1d20a1e7a059fab758fbc25985b
Nyholm <tobias.nyholm@gmail.com>
2014-01-15 18:52:37 +0100

    Updated example

--- examples/BasicAuthentication.php
+++ examples/BasicAuthentication.php
@@ -1,15 +1,18 @@
 <?php
 
 /**
- * This demonstrates how to authenticate
+ * This demonstrates how to authenticate with LinkedIn and send api requests
  */
 
-//First you need to make sure you've used composers auto load
+/*
+ * First you need to make sure you've used composers auto load. You have is probably 
+ * already done this before. You usually don't bother..
+ */
 //require_once "../vendor/autoload.php"
 
-$linkedIn=new \HappyR\LinkedIn\LinkedIn('app_id', 'app_secret');
+$linkedIn=new HappyR\LinkedIn\LinkedIn('app_id', 'app_secret');
 
-//if not athenticated
+//if not authenticated
 if (!$linkedIn->isAuthenticated()) {
     $url = $linkedIn->getLoginUrl();
     echo "<a href='$url'>Login with LinkedIn</a>";
@@ -17,6 +20,6 @@ if (!$linkedIn->isAuthenticated()) {
 }
 
 //we know that the user is authenticated now
-$user=$linkedIn->api('v1/people/~:(first-name,last-name)');
+$user=$linkedIn->api('v1/people/~:(firstName,lastName)');
 
-echo "Welcome ".$user['first-name'];
+echo "Welcome ".$user['firstName'];
