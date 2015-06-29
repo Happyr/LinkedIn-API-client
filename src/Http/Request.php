b7ea24104ec01f64de1bfbb22dec37004ecfac22
Sudhi Seshachala <sseshachala@users.noreply.github.com>
2015-06-28 21:48:22 -0500

    Create Request.php

---
+++ src/Http/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Happyr\LinkedIn\Http;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\TransferException;
+use Happyr\LinkedIn\Exceptions\LinkedInApiException;
+
+/**
+ * @author Tobias Nyholm
+ */
+class Request extends GuzzleRequest
+{
+
+}
